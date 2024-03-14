35bde56a60830cc83aba783cdb7a64c20b7794d3
Aura Kelloniemi <kaura.dev@sange.fi>
2024-03-14 16:08:33 +0200

    Initial commit

---
+++ src/Main.hs
@@ -0,0 +1,11 @@
+-- |
+-- Module      : Main
+-- Description : Entry point to Aural Maze
+-- Copyright   : Copyright (C) 2024 Aura Kelloniemi
+-- License     : GPL-3
+-- Maintainer  : kaura.dev@sange.fi
+-- Portability : GHC
+module Main where
+
+main :: IO ()
+main = pure ()
