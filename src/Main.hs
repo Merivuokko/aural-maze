09fc1de9f171199ec656d9577f2dd53ff345314b
Aura Kelloniemi <kaura.dev@sange.fi>
2024-03-22 15:35:48 +0200

    Simple main program
    It generates a music expression, prints and plays the synthesized version of it.

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,44 @@
 -- License     : GPL-3
 -- Maintainer  : kaura.dev@sange.fi
 -- Portability : GHC
-module Main where
+module Main (main) where
+
+import Data.Text.IO qualified as T
+import System.Random (initStdGen)
+
+import LambdaSound
+
+import RandomMusic.Generator
+import RandomMusic.Parameters
+import Scherzo.Format.LilyPond.Writer
+import Scherzo.Music.Elementary
+import Synthesis.Generator
+import Synthesis.Parameters
 
 main :: IO ()
-main = pure ()
+main = do
+    let musicParams =
+            MusicParams
+                { sequential =
+                    SequentialMusicParams
+                        { lengthRange = Range 3 3
+                        },
+                  note =
+                    NoteParams
+                        { pitchRange =
+                            Range
+                                { min = NotePitch {name = C, octave = 2, alteration = Natural},
+                                  max = NotePitch {name = C, octave = 6, alteration = Natural}
+                                }
+                        }
+                }
+        synthParams =
+            SynthParams
+                { tempoCrotchetsPerMinute = 180,
+                  baseTuning = 442.0
+                }
+    rng <- initStdGen
+    let music = generateMusic musicParams rng
+        sound = generateSignal synthParams music
+    T.putStrLn $! musicToLilyPond music
+    play 48000 1.0 sound
